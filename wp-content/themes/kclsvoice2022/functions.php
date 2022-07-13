e88e59f1cc881a84ce920d7253d9e22bd1e127fc
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-12 17:36:51 -0700

    Registers nav menus and actually calls the footer

--- wp-content/themes/kclsvoice2022/functions.php
+++ wp-content/themes/kclsvoice2022/functions.php
@@ -5,4 +5,14 @@ function kcls_voice_styles() {
     wp_enqueue_script('kcls-voice-custom-script', get_template_directory_uri() . '/assets/js/script.js', array(), filemtime(get_template_directory() . '/assets/js/script.js'), true );
 }
 
+function kcls_voice_register_menus() {
+    register_nav_menus(
+        array(
+            'header-menu' => __('Header Menu'),
+            'footer-menu' => __('Footer Menu')
+        )
+    );
+}
+
 add_action( 'wp_enqueue_scripts', 'kcls_voice_styles' );
+add_action( 'init', 'kcls_voice_register_menus' );
--- wp-content/themes/kclsvoice2022/index.php
+++ wp-content/themes/kclsvoice2022/index.php
@@ -1,2 +1,3 @@
 <?php get_header(); ?>
 <h1>Hello there</h1>
+<?php get_footer(); ?>
