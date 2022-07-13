7715c9aaff3ccdf3c032736bf7d678c3060c4c86
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-12 17:28:54 -0700

    Creates empty JavaScript file for future use and updates file version numbers in Functions.php to reflect last modified time of each file

--- wp-content/themes/kclsvoice2022/functions.php
+++ wp-content/themes/kclsvoice2022/functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function kcls_voice_styles() {
-    wp_enqueue_style( 'kcls-voice-main-css', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'kcls-voice-main-css', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), false);
+    wp_enqueue_script('kcls-voice-custom-script', get_template_directory_uri() . '/assets/js/script.js', array(), filemtime(get_template_directory() . '/assets/js/script.js'), true );
 }
 
 add_action( 'wp_enqueue_scripts', 'kcls_voice_styles' );
