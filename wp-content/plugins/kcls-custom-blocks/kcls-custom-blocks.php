092b004f05311d9ef36d475c26b40e51a701e0d7
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-18 13:56:39 -0700

    Removes unneeded boilerplate code from create-block script

--- wp-content/plugins/kcls-custom-blocks/kcls-custom-blocks.php
+++ wp-content/plugins/kcls-custom-blocks/kcls-custom-blocks.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Kcls Custom Blocks
- * Description:       Example static block scaffolded with Create Block tool.
+ * Plugin Name:       KCLS Voice Custom Blocks
+ * Description:       Custom blocks for KCLS Voice theme.
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           1.0.0
+ * Author:            Two Dogs Web Development
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       kcls-custom-blocks
@@ -13,13 +13,6 @@
  * @package           create-block
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_kcls_custom_blocks_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
