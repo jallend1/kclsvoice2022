12010bae944b9fd6e2094d95f491d4c81473f123
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-18 13:00:45 -0700

    Adds support for custom logo and integrates that support into the header

--- wp-content/themes/kclsvoice2022/functions.php
+++ wp-content/themes/kclsvoice2022/functions.php
@@ -1,7 +1,6 @@
 <?php
 
 function kcls_voice_styles() {
-    // Enqueues Google Font Inter
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Inter:400,700', array(), '1.0.0' );
     wp_enqueue_style( 'kcls-voice-main-css', get_stylesheet_uri(), ['google-fonts'], filemtime(get_template_directory() . '/style.css'), false);
     wp_enqueue_script('kcls-voice-custom-script', get_template_directory_uri() . '/assets/js/script.js', array(), filemtime(get_template_directory() . '/assets/js/script.js'), true );
@@ -16,5 +15,11 @@ function kcls_voice_register_menus() {
     );
 }
 
-add_action( 'wp_enqueue_scripts', 'kcls_voice_styles' );
+function kcls_voice_theme_setup(){
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+}
+
 add_action( 'init', 'kcls_voice_register_menus' );
+add_action( 'wp_enqueue_scripts', 'kcls_voice_styles' );
+add_action( 'after_setup_theme', 'kcls_voice_theme_setup' );
--- wp-content/themes/kclsvoice2022/header.php
+++ wp-content/themes/kclsvoice2022/header.php
@@ -11,7 +11,12 @@
     <header>
     <div class="logo">
         <a href="<?php echo home_url(); ?>">
-            <img src="<?php echo get_theme_file_uri('assets/images/local1857logo.png'); ?>" alt="KCLS Voice Logo" >
+            <?php 
+                if(has_custom_logo()) {
+                    the_custom_logo();
+                } else { ?>
+                    <img src="<?php echo get_theme_file_uri('assets/images/local1857logo.png'); ?>" alt="KCLS Voice Logo" >
+                <?php } ?>        
         </a>    
         </div>
         <?php wp_nav_menu( array( 'theme_location' => 'header-menu') ); ?>
