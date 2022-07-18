fdfead283118ca83a6a2c3e1b4ec6666e836c455
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-18 13:47:35 -0700

    Creates basic layout for header banner

--- wp-content/themes/kclsvoice2022/header.php
+++ wp-content/themes/kclsvoice2022/header.php
@@ -8,17 +8,32 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
-    <header>
-    <div class="logo">
-        <a href="<?php echo home_url(); ?>">
-            <?php 
-                if(has_custom_logo()) {
-                    the_custom_logo();
-                } else { ?>
-                    <img src="<?php echo get_theme_file_uri('assets/images/local1857logo.png'); ?>" alt="KCLS Voice Logo" >
-                <?php } ?>        
-        </a>    
-        </div>
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu') ); ?>
-    </header>
+    <nav>
+        <div class="logo">
+            <a href="<?php echo home_url(); ?>">
+                <?php 
+                    if(has_custom_logo()) {
+                        the_custom_logo();
+                    } else { ?>
+                        <img src="<?php echo get_theme_file_uri('assets/images/local1857logo.png'); ?>" alt="KCLS Voice Logo" >
+                    <?php } ?>        
+            </a>    
+            </div>
+            <div class="header-menu">
+                <?php wp_nav_menu( array( 'theme_location' => 'header-menu') ); ?>
+            </div>
+        </nav>
+        <header  class="kcls-main-header">
+            <div class="left-header">
+                <h1>Local 1857</h1>
+                <h2>King County Library System employees are stronger together.
+                </h2>
+                <div>
+                    <p>Local 1857 is part of the Washington State Council of County and City Employees (WSCCCE), often referred to as Council 2. Council 2 represents 17,000 public service employees across Washington State and Idaho, and is part of the American Federation of State, County, and Municipal Employees (AFSCME) a 1.6 million member Union with members across the United States</p>
+                </div>
+            </div>
+            <div class="right-header">
+                <button class="cta-button">Get involved</button>
+            </div>
+        </header>
     
