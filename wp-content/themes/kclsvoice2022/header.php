d66ea050d966e50ad382ac975af51610f265896e
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-21 18:44:59 -0700

    Moves communication icons up into the header itself

--- wp-content/themes/kclsvoice2022/header.php
+++ wp-content/themes/kclsvoice2022/header.php
@@ -33,7 +33,41 @@
                 </div>
             </div>
             <div class="right-header">
-                <button class="cta-button">Get involved</button>
+                <!-- <button class="cta-button">Get involved</button> -->
+                <div class="kcls-contact-boxes">
+        <div class="kcls-contact-box">
+            <div class="kcls-contact-box-title">
+                <h2>Newsletter</h2>
+            </div>
+            <div class="kcls-contact-box-image">
+                <img src="<?php echo get_theme_file_uri('assets/icons/email.svg'); ?>" />
+            </div>
+        </div>
+        <div class="kcls-contact-box">
+            <div class="kcls-contact-box-title">
+                <h2>Text Alerts</h2>
+            </div>
+            <div class="kcls-contact-box-image">
+                <img src="<?php echo get_theme_file_uri('assets/icons/chat.svg'); ?>" />
+            </div>
+        </div>
+        <div class="kcls-contact-box">
+            <div class="kcls-contact-box-title">
+                <h2>Watercooler</h2>
+            </div>
+            <div class="kcls-contact-box-image">
+                <img src="<?php echo get_theme_file_uri('assets/icons/watercooler.svg'); ?>" />
+            </div>
+        </div>
+        <div class="kcls-contact-box">
+            <div class="kcls-contact-box-title">
+                <h2>Find Your Steward</h2>
+            </div>
+            <div class="kcls-contact-box-image">
+                <img src="<?php echo get_theme_file_uri('assets/icons/people.svg'); ?>" />
+            </div>
+        </div>
+    </div>
             </div>
         </header>
     
--- wp-content/themes/kclsvoice2022/index.php
+++ wp-content/themes/kclsvoice2022/index.php
@@ -1,36 +1,3 @@
 <?php get_header(); ?>
-    <div class="kcls-contact-boxes">
-        <div class="kcls-contact-box">
-            <div class="kcls-contact-box-title">
-                <h2>Newsletter</h2>
-            </div>
-            <div class="kcls-contact-box-image">
-                <img src="<?php echo get_theme_file_uri('assets/icons/email.svg'); ?>" />
-            </div>
-        </div>
-        <div class="kcls-contact-box">
-            <div class="kcls-contact-box-title">
-                <h2>Text Alerts</h2>
-            </div>
-            <div class="kcls-contact-box-image">
-                <img src="<?php echo get_theme_file_uri('assets/icons/chat.svg'); ?>" />
-            </div>
-        </div>
-        <div class="kcls-contact-box">
-            <div class="kcls-contact-box-title">
-                <h2>Watercooler</h2>
-            </div>
-            <div class="kcls-contact-box-image">
-                <img src="<?php echo get_theme_file_uri('assets/icons/watercooler.svg'); ?>" />
-            </div>
-        </div>
-        <div class="kcls-contact-box">
-            <div class="kcls-contact-box-title">
-                <h2>Find Your Steward</h2>
-            </div>
-            <div class="kcls-contact-box-image">
-                <img src="<?php echo get_theme_file_uri('assets/icons/people.svg'); ?>" />
-            </div>
-        </div>
-    </div>
+    
 <?php get_footer(); ?>
