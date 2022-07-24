534a17484f4d13f5afe71007cdd75e00618af682
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-24 12:44:09 -0700

    Updates header background image to transparent and moves navbar on top of main banner image

--- wp-content/themes/kclsvoice2022/header.php
+++ wp-content/themes/kclsvoice2022/header.php
@@ -8,6 +8,7 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
+    <header class="kcls-main-header">
     <nav>
         <div class="logo">
             <a href="<?php echo home_url(); ?>">
@@ -23,5 +24,53 @@
                 <?php wp_nav_menu( array( 'theme_location' => 'header-menu') ); ?>
             </div>
         </nav>
+    <div class="kcls-main-header-content">
+        <div class="left-header">
+        <h1>Local 1857</h1>
+        <h2>King County Library System employees are stronger together.
+        </h2>
+        <div>
+            <p>When you signed your membership card, you became part of an employee Union that represents approximately 1,000 members at KCLS. Membership gives you the right to participate in decisions that affect your wages, hours, and working conditions. Union members participate in the democratic governance of the Union, and have access to member-only benefits.</p>
+        </div>
+    </div>
+    <div class="right-header">
+        <!-- <button class="cta-button">Get involved</button> -->
+        <div class="kcls-contact-boxes">
+            <div class="kcls-contact-box">
+                <div class="kcls-contact-box-title">
+                    <h2>Newsletter Signup</h2>
+                </div>
+                <div class="kcls-contact-box-image">
+                    <img src="<?php echo get_theme_file_uri('assets/icons/email.svg'); ?>" />
+                </div>
+            </div>
+            <div class="kcls-contact-box">
+                <div class="kcls-contact-box-title">
+                    <h2>Get Text Alerts</h2>
+                </div>
+                <div class="kcls-contact-box-image">
+                    <img src="<?php echo get_theme_file_uri('assets/icons/chat.svg'); ?>" />
+                </div>
+            </div>
+            <div class="kcls-contact-box">
+                <div class="kcls-contact-box-title">
+                    <h2>Watercooler</h2>
+                </div>
+                <div class="kcls-contact-box-image">
+                    <img src="<?php echo get_theme_file_uri('assets/icons/watercooler.svg'); ?>" />
+                </div>
+            </div>
+            <div class="kcls-contact-box">
+                <div class="kcls-contact-box-title">
+                    <h2>Find Your Steward</h2>
+                </div>
+                <div class="kcls-contact-box-image">
+                    <img src="<?php echo get_theme_file_uri('assets/icons/people.svg'); ?>" />
+                </div>
+            </div>
+        </div>
+                    </div>
+    </div>
+</header>
         
     
