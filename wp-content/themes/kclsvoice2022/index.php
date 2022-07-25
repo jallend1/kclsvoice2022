c46e5b890f2f4484db831b7c72c806377f4a3c77
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-25 16:58:58 -0700

    Adjusts style for rendering most recent blog posts

--- wp-content/themes/kclsvoice2022/index.php
+++ wp-content/themes/kclsvoice2022/index.php
@@ -10,22 +10,35 @@ $the_query = new WP_Query($args); ?>
 
 
 <div class="kcls-recent-posts">
-    <?php if ( $the_query->have_posts() ) : ?>
+    <?php if ( $the_query->have_posts() ) ?>
         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-        <div class="kcls-recent-post">
-            <header>
-                <h2><?php the_title(); ?></h2>
-                <p><?php the_time(); ?></p>
-            </header>
-            <?php if ( has_post_thumbnail() ) : ?>
-                <div class="kcls-recent-post-thumbnail">
-                    <?php the_post_thumbnail('medium'); ?>
+            <!-- If current post is most recent, applies unique styling -->
+            <?php 
+                if($the_query->current_post === 0) {?>
+                <div class="kcls-recent-post kcls-most-recent-post">
+            <?php } else { ?>
+                <div class="kcls-recent-post kcls-additional-post">
+                    <?php } ?>
+                <div class="left-column">
+                    <header> 
+                        <h2><?php the_title(); ?></h2>
+                        <p class="kcls-recent-post-time"><?php the_time(); ?></p>
+                    </header>
+                    <!-- Shows excerpt only for most recent post -->
+                    <?php if($the_query->current_post === 0) { ?>
+                        <main>
+                            <p><?php the_excerpt(); ?></p>
+                        </main>
+                    <?php } ?>
                 </div>
-            <?php endif; ?>
-            <p><?php the_excerpt(); ?></p>
-        </div>    
+                <div class="right-column">
+                    <?php if ( has_post_thumbnail() ) ?>
+                    <div class="kcls-recent-post-thumbnail">
+                        <?php the_post_thumbnail('medium'); ?>
+                    </div>
+                </div>
+            </div>    
         <?php endwhile; ?>
-    <?php endif; ?>
-</div>
-
+        </div>
+    <?php wp_reset_postdata(); ?>
 <?php get_footer(); ?>
