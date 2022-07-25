51a957f865c85a575389a570d0e3cff138d37d56
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-24 18:58:08 -0700

    Creates custom query to retrieve just the latest four posts

--- wp-content/themes/kclsvoice2022/index.php
+++ wp-content/themes/kclsvoice2022/index.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
-<h1>the blogs</h1>
+<!-- Get the latest four posts -->
+<?php $args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4,
+    'orderby' => 'date',
+    'order' => 'DESC'
+);
+$the_query = new WP_Query($args); ?>
+
+<!-- Show the results of the the_query -->
+<?php if ( $the_query->have_posts() ) : ?>
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+    <div>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+    </div>    
+    <?php endwhile; ?>
+<?php endif; ?>
+
 <?php get_footer(); ?>
