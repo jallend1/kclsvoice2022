3726cf2646a980ea1279fb32b58a33c9a429731b
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-26 16:37:39 -0700

    Adds blog page banner image to single post and publishing details

--- wp-content/themes/kclsvoice2022/single.php
+++ wp-content/themes/kclsvoice2022/single.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
-
+<?php while ( have_posts() ) : the_post(); ?>
+<header class="kcls-blog-banner">
+    <div class="kcls-blog-details">
+        <h1><?php the_title(); ?></h1>
+            <p class="kcls-publish-details">Posted on <?php the_date(); ?> by <?php the_author(); ?></p>
+        </div>
+</header>
 <main class="kcls-blog-post">
     <div class="kcls-blog-post-content">
-        <?php while ( have_posts() ) : the_post(); ?>
-        <div>
-            <h2><?php the_title(); ?></h2>
-        </div>        
-        <div>
             <?php the_content(); ?>
-        </div>
         <?php endwhile; ?>
     </div>
 </main>
