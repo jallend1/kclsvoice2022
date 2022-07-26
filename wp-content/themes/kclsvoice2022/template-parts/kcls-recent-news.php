a7a72e5a9bc37549519150472dcdc40d7aec8c51
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-07-25 17:27:38 -0700

    Creates temporary section heading for news area

--- wp-content/themes/kclsvoice2022/template-parts/kcls-recent-news.php
+++ wp-content/themes/kclsvoice2022/template-parts/kcls-recent-news.php
@@ -5,36 +5,40 @@
     'order' => 'DESC'
 );
 $the_query = new WP_Query($args); ?>
-
-<div class="kcls-recent-posts">
-    <?php if ( $the_query->have_posts() ) ?>
-        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-            <!-- If current post is most recent, applies unique styling -->
-            <?php 
-                if($the_query->current_post === 0) {?>
-                <div class="kcls-recent-post kcls-most-recent-post">
-            <?php } else { ?>
-                <div class="kcls-recent-post kcls-additional-post">
-                    <?php } ?>
-                <div class="left-column">
-                    <header> 
-                        <h2><?php the_title(); ?></h2>
-                        <p class="kcls-recent-post-time"><?php the_time(); ?></p>
-                    </header>
-                    <!-- Shows excerpt only for most recent post -->
-                    <?php if($the_query->current_post === 0) { ?>
-                        <main>
-                            <p><?php the_excerpt(); ?></p>
-                        </main>
-                    <?php } ?>
-                </div>
-                <div class="right-column">
-                    <?php if ( has_post_thumbnail() ) ?>
-                    <div class="kcls-recent-post-thumbnail">
-                        <?php the_post_thumbnail('medium'); ?>
+<div class="kcls-news">
+    <header class="kcls-section-title">
+        <h2 class="kcls-heading">Latest News</h2>
+    </header>
+    <div class="kcls-recent-posts">
+        <?php if ( $the_query->have_posts() ) ?>
+            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                <!-- If current post is most recent, applies unique styling -->
+                <?php 
+                    if($the_query->current_post === 0) {?>
+                    <div class="kcls-recent-post kcls-most-recent-post">
+                <?php } else { ?>
+                    <div class="kcls-recent-post kcls-additional-post">
+                        <?php } ?>
+                    <div class="left-column">
+                        <div> 
+                            <h2><?php the_title(); ?></h2>
+                            <p class="kcls-recent-post-time"><?php the_time(); ?></p>
+                        </div>
+                        <!-- Shows excerpt only for most recent post -->
+                        <?php if($the_query->current_post === 0) { ?>
+                            <main>
+                                <p><?php the_excerpt(); ?></p>
+                            </main>
+                        <?php } ?>
                     </div>
-                </div>
-            </div>    
-        <?php endwhile; ?>
-        </div>
-    <?php wp_reset_postdata(); ?>
+                    <div class="right-column">
+                        <?php if ( has_post_thumbnail() ) ?>
+                        <div class="kcls-recent-post-thumbnail">
+                            <?php the_post_thumbnail('medium'); ?>
+                        </div>
+                    </div>
+                </div>    
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+    </div>
+</div>
