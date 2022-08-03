26e35464aea5be0abc3fac400634c84c7d097a9a
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-08-02 17:37:20 -0700

    Now that more content has been built, adjusts front page template to show the actual post first

--- wp-content/themes/kclsvoice2022/front-page.php
+++ wp-content/themes/kclsvoice2022/front-page.php
@@ -1,36 +1,18 @@
 <?php get_header(); ?>
 <div class="kcls-front-page">
-    <div class="kcls-main-banner">
-        <div class="left-header">
-            <h1>Local 1857</h1>
-            <h2>King County Library System employees are stronger together.
-            </h2>
-            <div>
-                <p>When you signed your membership card, you became part of an employee Union that represents approximately 1,000 members at KCLS. Membership gives you the right to participate in decisions that affect your wages, hours, and working conditions. Union members participate in the democratic governance of the Union, and have access to member-only benefits.</p>
-            </div>
-        </div>
-        <div class="right-header">
-            <button class="kcls-cta-button">
-                <a href="#">
-                    <span>Get Involved</span>
-                </a>
-            </button>
-            <!-- <?php get_template_part('template-parts/kcls-contact-boxes'); ?> -->
-        </div>
-    </div>
-    <?php get_template_part('template-parts/kcls-recent-news'); ?>
+    <?php 
+    if ( have_posts() ) :
+        while ( have_posts() ) :
+            the_post();
+            ?>
+                <?php the_content(); ?>
+            <?php
+        endwhile;
+    endif; ?>
     <?php get_template_part('template-parts/kcls-contracts'); ?>
+    <?php get_template_part('template-parts/kcls-recent-news'); ?>
     </div>
     <?php get_footer(); ?>
 
 
-    <?php 
 
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		?>
-			<?php the_content(); ?>
-		<?php
-	endwhile;
-endif;
