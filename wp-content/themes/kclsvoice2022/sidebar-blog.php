6562b9499b76052c2edc4ee1803407be228f9543
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-08-08 12:33:14 -0700

    Adds background color and a container to the news posts on sidebar

--- wp-content/themes/kclsvoice2022/sidebar-blog.php
+++ wp-content/themes/kclsvoice2022/sidebar-blog.php
@@ -1,5 +1,6 @@
 <div class="kcls-blog-sidebar">
     <h2 class="kcls-blog-sidebar-heading">Recent News</h2>
+    <div class="kcls-blog-sidebar-posts">
     <?php 
         $latest_news = new WP_Query(array(
             'post_type' => 'post',
@@ -14,20 +15,22 @@
                 if($latest_news->current_post !== 0) :
                 ?>
                     <div class="kcls-sidebar-post">
-                        <a href="<?php the_permalink(); ?>">
-                            <h3><?php the_title(); ?></h3>
-                        </a>
-                            <div class="kcls-sidebar-post-thumbnail">
-                                <?php if ( has_post_thumbnail() ){
-                                    the_post_thumbnail('medium');
-                                } 
-                                // If no featured image, display the first image from the post or default logo
-                                else {
-                                    $kcls_post_img = kcls_get_post_image();
-                                    echo '<img src="' . $kcls_post_img . '" alt="' . get_the_title() . '">';
-                                } ?> 
-                            </div>
-                        <p class="kcls-recent-post-time"><?php the_date(); ?></p>    
+                        <div class="kcls-sidebar-post-thumbnail">
+                            <?php if ( has_post_thumbnail() ){
+                                the_post_thumbnail('medium');
+                            } 
+                            // If no featured image, display the first image from the post or default logo
+                            else {
+                                $kcls_post_img = kcls_get_post_image();
+                                echo '<img src="' . $kcls_post_img . '" alt="' . get_the_title() . '">';
+                            } ?> 
+                        </div>
+                        <div class="kcls-sidebar-text">
+                            <a href="<?php the_permalink(); ?>">
+                                <h3><?php the_title(); ?></h3>
+                            </a>
+                            <p class="kcls-recent-post-time"><?php the_date(); ?></p>    
+                        </div>
                     </div> 
                 <?php
                 endif;
@@ -35,4 +38,5 @@
         endif;
         wp_reset_postdata();
     ?>
+    </div>
 </div>
