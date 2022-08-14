60bc5b6ef3a1ae8949426d9435334b3d0bbbf0ac
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-08-14 13:28:16 -0700

    Creates a generic index page

--- wp-content/themes/kclsvoice2022/index.php
+++ wp-content/themes/kclsvoice2022/index.php
@@ -1,14 +1,17 @@
-<?php 
-
-get_header();
-
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post();
-		?>
-			<?php the_content(); ?>
-		<?php
-	endwhile;
-endif;
-
-get_footer();
+<?php get_header(); ?>
+<div class="kcls-page-banner">
+    <h1 class="kcls-page-title"><?php the_title(); ?></h1>
+</div>
+<div class="kcls-page-content">
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) :
+            the_post();
+            ?>
+                <?php the_content(); ?>
+            <?php
+        endwhile;
+    endif;
+    ?>
+</div>
+<?php get_footer();
