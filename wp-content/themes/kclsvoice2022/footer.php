bf23b4c79dceb7a177660e4a5e09d42ece6a21ae
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-12 15:53:00 -0800

    Expands footer out and incorporates Two Dogs company information

--- wp-content/themes/kclsvoice2022/footer.php
+++ wp-content/themes/kclsvoice2022/footer.php
@@ -1,17 +1,22 @@
     <footer class="kcls-footer">    
-        <div class="logo">
-            <a href="<?php echo home_url(); ?>">
-                <?php 
-                    if(has_custom_logo()) {
-                        the_custom_logo();
-                    } else { ?>
-                        <img src="<?php echo get_theme_file_uri('assets/images/local1857logo.png'); ?>" alt="KCLS Voice Logo" >
-                    <?php } ?>        
-            </a>    
-            </div>    
-        <nav class="kcls-footer-menu">
-                <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
-        </nav>
+        <div class="kcls-voice-footer">
+            <div class="logo">
+                <a href="<?php echo home_url(); ?>">
+                    <?php 
+                        if(has_custom_logo()) {
+                            the_custom_logo();
+                        } else { ?>
+                            <img src="<?php echo get_theme_file_uri('assets/images/local1857logo.png'); ?>" alt="KCLS Voice Logo" >
+                        <?php } ?>        
+                </a>    
+                </div>    
+            <nav class="kcls-footer-menu">
+                    <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+            </nav>
+        </div>
+        <div class="kcls-voice-designer-footer">
+            <p>Website created by <a href="https://twodogsdev.com/">Two Dogs Web Development</a>.</p>
+        </div>
     </footer>
     <?php wp_footer(); ?>
 </body>
