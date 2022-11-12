3af349eb2cbeac0c0bbe304857c14196c4f15151
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-12 10:55:54 -0800

    Modifies Laravel Mix to not process CSS URLs

--- wp-content/themes/kclsvoice2022/webpack.mix.js
+++ wp-content/themes/kclsvoice2022/webpack.mix.js
@@ -1,2 +1,4 @@
 let mix = require('laravel-mix');
-mix.sass('assets/scss/src /app.scss', 'assets/scss/dist');
+mix.sass('assets/scss/src/app.scss', 'assets/scss/dist').options({
+  processCssUrls: false
+});
