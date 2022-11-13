199ae8e4de3416b81b8161c1ae073dabe8340d32
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-13 14:45:34 -0800

    Modifies Main Banner styling on the editor screen

--- wp-content/plugins/kcls-custom-blocks/build/index.asset.php
+++ wp-content/plugins/kcls-custom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6851233e1503be4e4c04');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b3f3dceeb605ceb9cbcb');
