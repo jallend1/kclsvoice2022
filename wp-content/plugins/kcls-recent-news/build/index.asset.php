0fea94ca97bd9777be0e472c2f3841bc732f1757
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-17 18:27:35 -0800

    Restores button style to the News Header

--- wp-content/plugins/kcls-recent-news/build/index.asset.php
+++ wp-content/plugins/kcls-recent-news/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4f00ffe784f55c47701e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd3f9e63535d2a138b163');
