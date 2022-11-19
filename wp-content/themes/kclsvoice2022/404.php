30c0e9440ce1b0af1dfa17e5ff68f71bc9f30ef2
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-19 15:07:40 -0800

    Adds a return home button to 404 page

--- wp-content/themes/kclsvoice2022/404.php
+++ wp-content/themes/kclsvoice2022/404.php
@@ -2,5 +2,9 @@
 <div class="kcls-page-not-found">
     <h1>Cataloging Error</h1>
     <p>Sorry, but we couldn't find the page you were looking for.</p>
+    <button>
+        <a href="<?php echo home_url(); ?>">
+            Return Home</a>
+    </button>
 </div>
 <?php get_footer(); ?>
