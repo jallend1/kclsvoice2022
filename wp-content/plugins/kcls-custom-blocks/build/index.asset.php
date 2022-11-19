52cb1c9cd9671201613c7bd6c915cb5fc5607e92
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-19 12:32:52 -0800

    Adjusts image size in Editor for icons in Communications blocks

--- wp-content/plugins/kcls-custom-blocks/build/index.asset.php
+++ wp-content/plugins/kcls-custom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b7305077b2dc6e18553b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '04b44a660925305b3226');
