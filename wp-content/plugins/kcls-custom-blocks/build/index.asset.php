a89da98e27502ff2699e87e7561873181276be4e
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-19 13:08:07 -0800

    Forces Contract columns to wrap in Editor

--- wp-content/plugins/kcls-custom-blocks/build/index.asset.php
+++ wp-content/plugins/kcls-custom-blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '04b44a660925305b3226');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '76b98dae36fe48ceecfd');
--- wp-content/plugins/kcls-custom-blocks/src/blocks/kcls-know-your-contract.js
+++ wp-content/plugins/kcls-custom-blocks/src/blocks/kcls-know-your-contract.js
@@ -39,7 +39,9 @@ export default function KCLSKnowYourContract() {
 					[
 						[
 							"core/columns",
-							{ className: "kcls-contracts-columns" },
+							{
+								className: "kcls-contracts-columns",
+							},
 							[
 								[
 									"core/column",
