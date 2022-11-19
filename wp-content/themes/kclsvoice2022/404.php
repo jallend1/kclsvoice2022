45900e8e9f9a8a7388a7e00335f962997e046949
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-19 14:59:32 -0800

    Adds basic 404 page

---
+++ wp-content/themes/kclsvoice2022/404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<div class="kcls-page-not-found">
+    <h1>Cataloging Error</h1>
+    <p>Sorry, but we couldn't find the page you were looking for.</p>
+</div>
+<?php get_footer(); ?>
