afa6e01eeada0cc8b99dcb0d81181880c0246c76
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-11-20 14:57:09 -0800

    Incorporates iCalEasyReader to parse iCal data from public calendar

--- wp-content/plugins/kcls-events/build/index.asset.php
+++ wp-content/plugins/kcls-events/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2fdcdc1424cc4243aa83');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'bd3eb7e1cf570a61e333');
--- wp-content/plugins/kcls-events/kcls-events.php
+++ wp-content/plugins/kcls-events/kcls-events.php
@@ -13,7 +13,39 @@
  * @package           create-block
  */
 
-function create_block_kcls_events_block_init() {
-	register_block_type( __DIR__ . '/build' );
+include 'iCalEasyReader.php';
+
+function kcls_get_events($url){
+	$ical = new iCalEasyReader();
+	$lines = $ical->load(file_get_contents($url));
+	return $lines['VEVENT'];
 }
+
+
+ function kcls_events_block_renderer($attr){
+	$events = kcls_get_events('https://calendar.google.com/calendar/ical/1857comms%40gmail.com/public/basic.ics');	
+	// print_r(kcls_get_events('https://calendar.google.com/calendar/ical/1857comms%40gmail.com/public/basic.ics'));
+	ob_start();
+	?>
+	<div class="kcls-event-container">
+		<?php foreach($events as $event): ?> 
+			<div class="kcls-event">
+				<h3><?php echo $event['SUMMARY']; ?></h3>
+				<p>What: <?php echo $event['DESCRIPTION']; ?></p>
+				<p>When: <?php echo $event['DTSTART']; ?></p>
+				
+				<p>Until: <?php echo $event['DTEND']; ?></p>
+			</div>
+		<?php endforeach; ?>
+	</div>
+	<?php
+	return ob_get_clean();
+}
+
+ function create_block_kcls_events_block_init() {
+	register_block_type( __DIR__ . '/build', [
+		'render_callback' => 'kcls_events_block_renderer',
+	] );
+}
+
 add_action( 'init', 'create_block_kcls_events_block_init' );
